93d28df4b492844d03bc0c8a2a4a1eb4829f4b78
Brayan-Salazarr <brayansalazzar@gmail.com>
2025-11-18 16:22:22 -0500

    I add header component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Nav } from './shared/components/nav/nav';
+import { Header } from './shared/components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Nav],
+  imports: [RouterOutlet, Nav, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
