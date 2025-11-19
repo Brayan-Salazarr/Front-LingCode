fbd09cd65d9a95a4772469dc439774978c51a86e
LauraAG98 <lauraguirregarcia@outlook.com>
2025-11-18 22:40:46 -0500

    Add media queries

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginRegistro } from './login-registro/login-registro';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,LoginRegistro],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
