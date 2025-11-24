36458ea7861d9ebea315561698915686d5ae7bc8
LauraAG98 <lauraguirregarcia@outlook.com>
2025-11-23 22:44:51 -0500

    MODIFIED: html, css, ts

--- src/app/login-registro/login-registro.ts
+++ src/app/login-registro/login-registro.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login-registro',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './login-registro.html',
   styleUrl: './login-registro.css',
 })
 export class LoginRegistro {
+  showLogin:Boolean=false;
 
+  toggleAuth():void{
+    this.showLogin=!this.showLogin;
+  }
 }
