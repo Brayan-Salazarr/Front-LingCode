001c69062ea6c8c4c937d970af3689a281ed77a8
Brayan-Salazarr <brayansalazzar1221@gmail.com>
2025-12-02 22:22:50 -0500

    I create a teaching method component and modify the mission component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { UnregisteredHome } from './unregistered-home/unregistered-home';
 import { Mission } from './mission/mission';
+import { TeachingMethod } from './teaching-method/teaching-method';
 
 export const routes: Routes = [
     { path: '', component: UnregisteredHome},
-    { path: 'mission', component: Mission}
+    { path: 'mission', component: Mission},
+    { path: 'teaching-method', component: TeachingMethod}
 ];
