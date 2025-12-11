003e1a28d58d7dddcc8e097e838364be11982444
LauraAG98 <lauraguirregarcia@outlook.com>
2025-12-11 14:13:24 -0500

    ADDED routes and ts NewPassword

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { NewPassword } from './new-password/new-password';
+import { LoginRegistro } from './login-registro/login-registro';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'login-registro', component:LoginRegistro},
+    {path: 'new-password', component:NewPassword},
+    {path: '', redirectTo: 'login-registro', pathMatch: 'full'}
+];
