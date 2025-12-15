a994020fc10325b385f69b3fd43998d9058437e0
Brayan-Salazarr <brayansalazzar@gmail.com>
2025-12-15 17:35:28 -0500

    I add changes to the registered components home

--- src/app/registered-home/registered-home.ts
+++ src/app/registered-home/registered-home.ts
@@ -3,10 +3,18 @@ import { Nav } from '../shared/components/nav/nav';
 import { Header } from '../shared/components/header/header';
 import { Footer } from '../shared/components/footer/footer';
 import { AuthService, User } from '../auth/services/authService';
+import { CommonModule } from '@angular/common';
+
+interface Practica {
+  modulo: string;
+  resultado: string;
+  fecha: string;
+  estado: 'completado' | 'pendiente';
+}
 
 @Component({
   selector: 'app-registered-home',
-  imports: [Nav, Header, Footer],
+  imports: [Nav, Header, Footer, CommonModule],
   templateUrl: './registered-home.html',
   styleUrl: './registered-home.css',
 })
@@ -18,4 +26,20 @@ export class RegisteredHome {
       this.user = user;
     });
   }
+
+  practicas: Practica[] = [
+    {
+      modulo: 'GitHub',
+      resultado: '75%',
+      fecha: '09/12/2026',
+      estado: 'completado' // Usaremos esto para el estilo (borde magenta/negro)
+    },
+    {
+      modulo: 'MySQL',
+      resultado: '0%',
+      fecha: '00/00/0000',
+      estado: 'pendiente' // Usaremos esto para el estilo
+    }
+    // Puedes agregar más objetos aquí y se generarán más tarjetas automáticamente
+  ];
 }
