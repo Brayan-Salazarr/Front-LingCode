181463ffb8997c58120e9c9a6c9a33de93427874
LauraAG98 <lauraguirregarcia@outlook.com>
2025-12-15 21:47:28 -0500

    Modificado nav de editar perfil.

--- src/app/about-us/about-us.ts
+++ src/app/about-us/about-us.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { Nav } from '../shared/components/nav/nav';
+import { Footer } from '../shared/components/footer/footer';
+import { Header } from '../shared/components/header/header';
 
 @Component({
   selector: 'app-about-us',
-  imports: [],
+  imports: [Nav, Header,Footer],
   templateUrl: './about-us.html',
   styleUrl: './about-us.css',
 })
