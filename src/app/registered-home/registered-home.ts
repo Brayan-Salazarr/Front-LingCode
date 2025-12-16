2c77eef5fbe83cf7d45ef9617e76cb60e582d4e1
Brayan-Salazarr <brayansalazzar1221@gmail.com>
2025-12-16 00:30:19 -0500

    I add achievements and subscriptions in the registered home component

--- src/app/registered-home/registered-home.ts
+++ src/app/registered-home/registered-home.ts
@@ -12,6 +12,12 @@ interface Practica {
   estado: 'completado' | 'pendiente';
 }
 
+interface Logro {
+  icono: string; // Clase CSS para el ícono (ej: fa-check, fa-fire, ruta de imagen)
+  mensaje: string;
+  bordeColor: string; // Color para el borde de neón (ej: '#00ffff')
+}
+
 @Component({
   selector: 'app-registered-home',
   imports: [Nav, Header, Footer, CommonModule],
@@ -41,5 +47,28 @@ export class RegisteredHome {
       estado: 'pendiente' // Usaremos esto para el estilo
     }
     // Puedes agregar más objetos aquí y se generarán más tarjetas automáticamente
+  ];
+
+  logros: Logro[] = [
+    {
+      icono: 'https://res.cloudinary.com/ddvjgyi3f/image/upload/v1765857108/image-removebg-preview_6_1_jcpsse.png', // Asumiendo que usarás algún sistema de íconos o rutas
+      mensaje: '¡Cumpliste con el objetivo semanal!',
+      bordeColor: '#00ffff'
+    },
+    {
+      icono: 'https://res.cloudinary.com/ddvjgyi3f/image/upload/v1765857109/image-removebg-preview_1_mx3asn.png',
+      mensaje: '¡Lograste una racha de 3 días!',
+      bordeColor: '#00ffff'
+    },
+    {
+      icono: 'https://res.cloudinary.com/ddvjgyi3f/image/upload/v1765857109/image-removebg-preview_4_1_xggd4v.png',
+      mensaje: '¡Completaste el módulo de GitHub!',
+      bordeColor: '#00ffff'
+    },
+    {
+      icono: 'https://res.cloudinary.com/ddvjgyi3f/image/upload/v1765857110/image-removebg-preview_5_1_a2scpd.png', 
+      mensaje: '¡Hiciste 5 actividades sin errores!',
+      bordeColor: '#00ffff'
+    }
   ];
 }
