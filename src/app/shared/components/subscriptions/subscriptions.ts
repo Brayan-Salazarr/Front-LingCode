136a4133544572b24967df4d07f72469058f967d
Brayan-Salazarr <brayansalazzar1221@gmail.com>
2026-02-15 15:54:20 -0500

    I add media queries in the payment method component

--- src/app/shared/components/subscriptions/subscriptions.ts
+++ src/app/shared/components/subscriptions/subscriptions.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-subscriptions',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './subscriptions.html',
   styleUrl: './subscriptions.css',
 })
