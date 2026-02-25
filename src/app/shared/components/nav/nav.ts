42535d6f09c211ef8ecb71f2fb314558c6e5a4b1
LauraAG98 <lauraguirregarcia@outlook.com>
2026-02-25 09:48:38 -0500

    Comments added to shared component: Nav.

--- src/app/shared/components/nav/nav.ts
+++ src/app/shared/components/nav/nav.ts
@@ -10,44 +10,56 @@ import { AuthService, User } from '../../../auth/services/authService';
   styleUrl: './nav.css',
 })
 export class Nav {
+  /*Permite recibir una clase o color desde el componente padre para cambiar el fondo del navegador*/
   @Input() colorBackground = '';
 
+  /*Variable que almacena el usuario actual.
+  Será null si el usuario no ha iniciado sesión*/
   user: User | null = null;
 
   constructor(
-    private router: Router,
-    public authService: AuthService
+    private router: Router, //Servicio para navegar entre rutas
+    public authService: AuthService //Servicio de autenticación
   ) {
+    /*Se suscribe al observable del usuario actual. Cada vez que el usuario cambia (login/logout), esta variable se actualiza automáticamente*/
     this.authService.currentUser$.subscribe(user => {
       this.user = user;
     });
   }
 
+  /*Navega a la página de login. Se envía el parámetro "view=login" para indicar que debe mostrarse la vista de iniciar sesión*/
   goLogin() {
     this.router.navigate(
       ['/login-registro'], { queryParams: { view: 'login' } }
     );
   }
 
+  /*Cierra la sesión del usuario y lo redirige al incio público*/
   logout() {
-    this.authService.logout();
-    this.router.navigate(['/']);
+    this.authService.logout(); //Elimina la sesión del usuario
+    this.router.navigate(['/']); //Redirige al home público
   }
 
+  /*Navega al home dependiendo si el usuario está autenticado o no*/
   goHome() {
     if (this.authService.isAuthenticated()) {
+      /*Si el usuario está autenticado, lo lleva al home de usuarios registrados*/
       this.router.navigate(['/registered-home']);
     } else {
+      /*Si no está autenticado, lo lleva al home público*/
       this.router.navigate(['/']);
     }
   }
 
+  /*Controla si el menú tipo hamburguesa está abierto o cerrado*/
   menuOpen = false;
 
+  /*Alterna el estado del menú tipo hamburguesa*/
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
   }
 
+  /*Cierra el menú tipo hamburguesa*/
   closeMenu() {
     this.menuOpen = false;
   }
