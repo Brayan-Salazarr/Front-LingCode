f539aa25a0fe2ffbe122f1a64423a0b4e94527a1
Brayan-Salazarr <brayansalazzar@gmail.com>
2026-02-27 17:51:19 -0500

    A fix has been added to the service components for connecting to the backend.

--- src/app/components/module-view/module-view.ts
+++ src/app/components/module-view/module-view.ts
@@ -6,6 +6,8 @@ import { CommonModule } from '@angular/common';
 import { AuthService, User } from '../../auth/services/authService';
 import { Module, ModuleService } from '../../service/moduleService';
 import { Router } from '@angular/router';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 interface Step {
   label: number;
@@ -23,66 +25,37 @@ interface ModuleViewModel extends Module {
   selector: 'app-module-view',
   imports: [Nav, Header, Footer, CommonModule],
   templateUrl: './module-view.html',
-  styleUrl: './module-view.css',
+  styleUrls: ['./module-view.css'],
 })
 export class ModuleView {
 
 
   user: User | null = null;
-  modules: ModuleViewModel[] = []; // ahora vienen del backend
-
-  //CONFIGURACIÓN VISUAL (SOLO FRONT)
-  visualConfig = [
-    {
-      image: 'https://res.cloudinary.com/ddvjgyi3f/image/upload/v1763696348/Group_25_fnpomn.png',
-      bgImage: 'https://res.cloudinary.com/ddvjgyi3f/image/upload/v1765929029/image-removebg-preview_16_2_ag1deb.png',
-      size: '',
-    },
-    {
-      image: 'https://res.cloudinary.com/ddvjgyi3f/image/upload/v1763695172/mysql-removebg-preview-removebg-preview_2_hc65ln.png',
-      bgImage: 'https://res.cloudinary.com/ddvjgyi3f/image/upload/v1765929038/image-removebg-preview_16_1_ava5nx.png',
-      size: 'size-img',
-    },
-    {
-      image: 'https://res.cloudinary.com/ddvjgyi3f/image/upload/v1763696367/Group_31_mmwojn.png',
-      bgImage: 'https://res.cloudinary.com/ddvjgyi3f/image/upload/v1765929029/image-removebg-preview_16_2_ag1deb.png',
-      size: '',
-    }
-  ];
+  modules$!: Observable<ModuleViewModel[]>; // ahora vienen del backend
+  currentStep = 1;
 
   constructor(
     public authService: AuthService,
     private moduleService: ModuleService,
     private router: Router
-  ) {
-    this.authService.currentUser$.subscribe(user => {
-      this.user = user;
-    });
+  ) { }
+
+  ngOnInit() {
+    this.modules$ = this.moduleService.getModules().pipe(
+      map(data =>
+        data
+          .filter(m => m.is_published)
+          .map(module => ({
+            ...module,
+            image: module.thumbnail_url || '',
+            bgImage: 'https://res.cloudinary.com/ddvjgyi3f/image/upload/v1765929029/image-removebg-preview_16_2_ag1deb.png',
+            size: '',
+            text: 'Progreso',
+            progress: 0
+          }))
+      )
+    );
   }
-
-currentStep = 1;
-
-  ngOnInit(): void {
-    this.moduleService.getModules().subscribe(data => {
-      console.log("DATA BACK:", data);
-      const published = data.filter(m => m.is_published);
-
-      this.modules = published.map((module, index) => ({
-        ...module,
-
-        // 🎨 solo visual
-        image: this.visualConfig[index]?.image || '',
-        bgImage: this.visualConfig[index]?.bgImage || '',
-        size: this.visualConfig[index]?.size || '',
-        text: 'Progreso',
-
-        // 👇 si el backend no manda progreso aún
-        progress: 0
-      }));
-
-    });
-  }
-
   goToLessons(moduleId: string) {
     this.router.navigate(['/modules', moduleId, 'lessons']);
   }
